9df21f0b3fae0445ea35263e7299b4cff06caaf0
Daniela Rocha <danirocha.rmd@gmail.com>
2021-03-19 14:59:23 -0300

    refactor: implement the basic logic of post a new user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,19 @@
+import UserService from '../services/UserService';
+
 class UserController {
     constructor () {
-        // -
+        this.service = UserService;
     }
 
     post (req, res) {
+        const { name, email, cpf, password } = req.body;
+      
+        const user = this.service.getUserByCPF(cpf);
+      
+        if (user) res.json({ message: 'This user already exists' });
+      
+        this.service.registerUser({ name, email, cpf, password });
+        
         res.json({ message: 'User successfully registered' });
       }
 }
---
+++ src/services/UserService.js
@@ -0,0 +1,15 @@
+class UserService {
+    constructor () {
+        // -
+    }
+
+    getUserByCPF(cpf) {
+        return cpf == "15312561711";
+    }
+
+    registerUser(newUser) {
+        return;
+    }
+}
+
+export default new UserService();
