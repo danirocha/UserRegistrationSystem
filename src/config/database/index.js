8a466b8f0dc92e831ec1b9fba4f038cc62f320d8
Daniela Rocha <danirocha.rmd@gmail.com>
2021-04-02 20:41:06 -0300

    refactor: fill in new users' IDs in the DB

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -4,6 +4,12 @@ import { Config } from 'node-json-db/dist/lib/JsonDBConfig';
 class Database {
     constructor () {
         this.db = new JsonDB(new Config("myDataBase"));
+
+        this.db.count = (rootElem) => {
+            const array = this.db.getData(`/${rootElem}`);
+
+            return array.length;
+        }
     }
 
     select(rootElem, property, value) {
@@ -19,6 +25,13 @@ class Database {
     }
 
     insert(rootElem, insertObj) {
+        try {
+            let elemsCount = this.db.count(rootElem);
+            insertObj.id = ++elemsCount;
+        } catch (err) {
+            insertObj.id = 1;
+        }
+
         this.db.push(`/${rootElem}[]`, insertObj, true);
     }
 
