97e0fd65d11241ec40a25e02c9fd16f9d75e3ca7
Daniela Rocha <danirocha.rmd@gmail.com>
2021-05-14 17:33:50 -0300

    refactor: starts validating inputs of AuthController

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,24 +1,35 @@
 import jwt from 'jsonwebtoken';
 import authConfig from '../config/auth';
 import UserService from '../services/UserService';
+import * as yup from 'yup';
 
 class AuthController {
     constructor () {
        this.userService = UserService;
+       this.loginSchema = yup.object().shape({
+            email: yup.string().email().required(),
+            password: yup.string().required()
+       });
     }
 
-    store (req, res) {
-        const { email, password } = req.body;
-        const user = this.userService.list({ email });
-      
-        if (!user || (user && password != user.password)) { // TODO: use bcrypt for the password
-            return res.sendResponse({ status: 400, data: { message: 'invalid login data' }});
-        }
-
-        const { id, name } = user;
-        const token = jwt.sign({ id }, authConfig.secret, authConfig.options);
+    async store (req, res) {
+        try {
+            await this.loginSchema.validate(req.body, { abortEarly: false });
+            
+            const { email, password } = req.body;
+            const user = this.userService.list({ email });
         
-        return res.sendResponse({ status: 200, data: { user: { name, email }, token }});
+            if (!user || (user && password != user.password)) { // TODO: use bcrypt for the password
+                return res.sendResponse({ status: 400, data: { message: 'invalid login data' }});
+            }
+
+            const { id, name } = user;
+            const token = jwt.sign({ id }, authConfig.secret, authConfig.options);
+            
+            return res.sendResponse({ status: 200, data: { user: { name, email }, token }});
+        } catch (err) {
+            return res.sendResponse({ status: 422, data: { message: 'invalid login data', errors: err.errors }});
+        };
       }
 }
 
