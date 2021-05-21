2ea0abea077797161859394dcdaf4862459d2a3b
Daniela Rocha <danirocha.rmd@gmail.com>
2021-05-21 14:50:59 -0300

    feat: adds user register confirmation

--- src/controllers/UserConfirmationController.js
+++ src/controllers/UserConfirmationController.js
@@ -1,12 +1,30 @@
 import UserConfirmationService from '../services/UserConfirmationService';
+import UserService from '../services/UserService';
 
 class UserConfirmationController {
     constructor () {
-        this.service = UserConfirmationService;
+        this.UserConfirmationService = UserConfirmationService;
+        this.UserService = UserService;
     }
 
     update (req, res) {
-        return res.sendResponse({status: 200, data: { message: 'User registration confirmed' } });
+        const { token } = req.params;
+        const userConfirmation = this.UserConfirmationService.list({ token });
+
+        if (!userConfirmation || !userConfirmation.userId) {
+            return res.sendResponse({status: 422, data: { message: 'Invalid data' } });
+        }
+
+        const tokenExpiration = new Date(userConfirmation.expiresAt);
+        const currentDate = new Date();
+
+        if (currentDate > tokenExpiration) {
+            return res.sendResponse({status: 422, data: { message: 'Token is expired' } });
+        }
+
+        const confirmedUser = this.UserService.update(userConfirmation.userId, { isConfirmed: true });
+
+        return res.sendResponse({status: 200, data: { message: 'User registration confirmed', data: confirmedUser } });
     }
 }
 
